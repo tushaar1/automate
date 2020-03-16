7d451b781ea224b893c30c7e5becf46542bc1a76
tushaar <kaushik.tushaar@gmail.com>
2020-03-16 10:44:52 -0400

    chapter 1

---
+++ chapter-one.py
@@ -0,0 +1,42 @@
+print('')
+'''
+operators:
+** - exponent
+% - modulus/remainder
+// - integer division floored division
+/ - division
+* - multiplication
+- subtraction
++ - subtraction
+pemdas - order of operation
+
+data types - category for values 
+integer - whole numbers
+float - decimal point
+strings - letters
+'''
+
+'''string concatenation'''
+'''
++ with strings is when you add two different strings together 
+'''
+# example
+print('alice'+'bob')
+# cant add string plus int
+# muliply string
+print('alice' * 5)
+
+# variables
+'''
+a variable is when you give something a value 
+x = 10 
+x is the variable with the value of ten.
+this can be changed whenever, a variable is initialised when you first give it a 
+value
+
+variables have naming values too
+cant start with a number
+cant contain a space 
+only uses letters number and the _ 
+they are case sensitive 
+'''
